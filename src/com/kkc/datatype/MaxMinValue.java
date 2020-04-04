e66836974876b5744920ab45ece1f752caa426ca
unknown <kishankc949@gmail.com>
2020-04-04 10:31:53 +0545

    My secondf proj

---
+++ src/com/kkc/datatype/MaxMinValue.java
@@ -0,0 +1,42 @@
+package com.kkc.datatype;
+
+public class MaxMinValue {
+	public static void main(String[]args){
+		int minValue = Integer.MIN_VALUE;
+		byte maxvalue= Byte.MAX_VALUE;
+		System.out.println(maxvalue);
+		short maxvalue1= Short.MAX_VALUE;
+		System.out.println(maxvalue1);
+		long maxvalue2=Long.MAX_VALUE;
+		System.out.println(maxvalue2);
+		int a =74738;
+		char chr='a';
+		String abc= "Kishor Kc";
+		if (a>5000 && a<100000){
+		System.out.println(true);
+	}
+		
+		else{
+			System.out.println(false);
+
+		}
+		for(int i=0;i<10;i++){
+			System.out.println("hello");
+			
+		}
+
+		
+		
+		int maxValue = Integer.MAX_VALUE;
+		System.out.println("Minimum value is =" +minValue);
+		System.out.println("Maximum value is =" +maxValue);
+	
+		savedata();
+		
+	}
+	public static void savedata(){
+		int abc=20;
+		System.out.println(abc);
+	}
+
+}
